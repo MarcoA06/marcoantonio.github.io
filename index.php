5325371b9f118f3f7b29c5f6483cb23e5b6b4967
MarcoA06 <38164997+MarcoA06@users.noreply.github.com>
2020-06-17 16:21:07 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="pt_Br">
+<head>
+  
+  <!-- Metas -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta property="og:locale" content="pt_BR">
+  <meta property="og:url" content="">
+  <meta property="og:title" content="">
+  <meta property="og:site_name" content="">
+  <meta property="og:description" content="">
+
+  <!-- Artigo -->
+  <meta property="og:type" content="article">
+  <meta property="article:author" content="Marco Antonio">
+  <meta property="article:section" content="">
+  <meta property="article:tag" content="">
+  <meta property="article:published_time" content="date_time">
+
+  <!-- Descrição da Página -->
+  <meta name="description" content="">
+  <meta name="author" content="Marco Antonio">
+
+  <!-- Palavras Chaves -->
+  <meta name="keywords" content="">
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Canonical Link -->
+  <link rel="canonical" href="" />
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="css/home.css" rel="stylesheet">
+
+  <!--Fontes do Google-->
+  <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Bebas+Neue|Luckiest+Guy|Orbitron|Poiret+One&display=swap" rel="stylesheet">
+
+  <link rel="icon" href="img/favicon.png" />
+
+  <title>Gynomo | Aprenda a Gerar Valor!</title>
+
+</head>
+<body>
+
+<!-- Inicio -->
+<nav class="navbar fixed-top">
+  <h2 class="nav">Gynomo</h1>
+    <ul class="nav">
+       <li class="nav-item">
+          <a href="#" class="nav-link">Planos</a>
+       </li>
+
+       <li class="nav-item">
+          <a href="#" class="nav-link">Sobre</a>
+       </li>
+
+       <li class="nav-item">
+          <a href="#" class="nav-link">Contato</a>
+       </li>
+
+    </ul>
+</nav>
+
+<section>
+
+  <div class="topo"></div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#F7E8A4" fill-opacity="1" d="M0,256L1440,96L1440,0L0,0Z"></path>
+  </svg>
+
+ <div class="centralizarRight">
+     <img src="img/gere.png" width="100%" height="100%">
+ </div>
+
+
+</section>
+
+<!-- Main -->
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 240">
+  <path fill="#F7E8A4" fill-opacity="1" d="M0,64L1440,224L1440,320L0,320Z"></path>
+</svg>
+<section class="main"></section>
+
+
+<!-- Rodape -->
+<section class="rodape">
+
+  <div class="centralizar">
+    <i class="fab fa-facebook"></i>
+    <i class="fab fa-instagram"></i>
+    <i class="fab fa-linkedin"></i>
+  </div>
+
+  <!-- Centralizar texto -->
+  <div class="centralizarTxt">
+    <p>&copy; By Gynomo | Gere Valor!</p>
+  </div>
+</section>
+
+
+
+
+</body>
+</html>
